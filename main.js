427035c37c754a2a172b1119ce316ad33e9678a6
Shimchik <ann.loshakova.111@gmail.com>
2018-10-31 20:39:19 +0300

    output non-repeating items

--- main.js
+++ main.js
@@ -1,39 +1,23 @@
 
-var obj1={name:"Vasia", age:"14"};
-var obj2={name:"Kolia", age:"41"};
-var obj3={name:"Sasha", age:"25"};
-var obj4={name:"Ira", age:"10"};
-var arr=[{name:"Vasia", age: 41}, {name:"Kolia", age: 5}, {name:"Sasha", age: 1}, {name:"Ira", age: 10}];
+var arr = [0,2,5,7,3,2,5,5,6];
+var arr2= new Array();
+var flag;
 
 console.log(arr);
 
-function sort (arr) {
-    tmp = [];
-    for (var i = 0; i < arr.length; i += 1) {
-        tmp.push(arr[i]);
+console.log("before " );//+ arr2);
+
+console.log(arr2);
+for (let i = 0; i < arr.length; i++) {
+    flag  = true;
+    for (let j = 0; j < arr2.length; j++) {
+        if (arr[i] == arr2[j])
+            flag = false;
+        
     }
-    console.log('tmp', tmp);
-    tmp.sort(function (a,b) {
-        return (a.age<b.age)? -1 : 1;
-    });
-    console.log('tmp', tmp);
-    return tmp;
+    if (flag)
+        arr2.push(arr[i]);
+    
 }
-
-/*for (var j = 0; j < arr.length -1; j += 1) {
- for (var i =0; i < arr.length - 1; i += 1) {
-
-    if (arr[i].age > arr[i + 1].age) {
-        var temp = Object.assign({}, arr[i]);
-        arr[i] = Object.assign({}, arr[i + 1]);
-        arr[i + 1] = Object.assign({}, temp);   
-    }
-}
-}*/
-/*
-arr.sort(function (a,b) {
-    return (a.age<b.age)? -1 : 1;
-});
-*/
-
-console.log(sort(arr));
+console.log("after " );
+console.log(arr2);
