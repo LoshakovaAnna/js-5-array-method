93fbb6cecddd720c461fc050c0482a032f3eec46
Shimchik <ann.loshakova.111@gmail.com>
2018-10-31 20:54:05 +0300

    use includes (not use flag)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var arr = [0,2,5,7,3,2,5,5,6];
+var arr = [0,2,5,7,3,2,5,5,6,10,1,1,1,1,];
 var arr2= new Array();
 var flag;
 
@@ -7,17 +7,11 @@ for (var i = 0; i < arr.length; i += 1) {
     document.write(arr[i] + ",    ");
 }
 
-for (let i = 0; i < arr.length; i++) {
-    flag  = true;
-    for (let j = 0; j < arr2.length; j++) {
-        if (arr[i] == arr2[j])
-            flag = false;
-        
-    }
-    if (flag)
-        arr2.push(arr[i]);
-    
+for (let i = 0; i < arr.length; i++) {   
+    if (!arr2.includes(arr[i]))
+        arr2.push(arr[i]);    
 }
+
 document.write("<br \/>  = не повторяющиеся элементы =  <br \/>");
 for (var i = 0; i < arr2.length; i += 1) {
     document.write(arr2[i] + ",    ");
