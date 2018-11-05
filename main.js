3b0fcf5bb28ae2929af87ac35c39b4b324f0dd33
Shimchik <ann.loshakova.111@gmail.com>
2018-11-05 19:43:32 +0300

    add toLowerCase

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 var string = prompt("введите строку для проверки");
-var string1 = string.replace(/ /g, '');
+var stringPreparedChecking= string.replace(/ /g, '').toLowerCase();
 
 var isPolynom = true;
 
-var j = string1.length;
+var j = stringPreparedChecking.length;
 for (let i = 0; i < j/2; i++) {
-    if (string1[i]!==string1[j-1-i]){
+    if (stringPreparedChecking[i]!==stringPreparedChecking[j-1-i]){
         isPolynom = false;
     break;
     }
 }
-console.log(string1);
 
 if (isPolynom){ 
     alert("Да! Это полиндром");
